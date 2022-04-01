b5976c189748e92439a137f1ea76815e53ba4f44
Matt Filetto <matt.filetto@gmail.com>
2022-04-01 13:58:17 -0700

    rro_overlay: enable components for ambient music detection to work
    * These are disabled globally and enabled on a device by device basis
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I49aa06c7cb928b4c0f953c22cf28de2222c54adc
    
    # Conflicts:
    #	device-lineage.mk

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,14 +1,15 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/raviole/overlay-lineage
 
+# RRO Overlays
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay \
+    NowPlayingOverlay
+
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
-
-# EUICC
-PRODUCT_PACKAGES += \
-    EuiccSupportPixelOverlay
 
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
