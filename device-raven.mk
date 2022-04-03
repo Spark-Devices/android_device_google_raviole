8ba616df9b05834528ccefe77865b62f1f71cef0
Matt Filetto <matt.filetto@gmail.com>
2022-04-03 07:28:30 -0700

    raven: lower padding threshold for keyboard in portrait.
    * The keys are abnormally narrow when like this.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I0d6c72990a5e2af27f232e7596d31b7b0fad7817

--- device-raven.mk
+++ device-raven.mk
@@ -200,8 +200,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 
 # Keyboard side padding in dp for portrait mode
-PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.kb_pad_port_r=11
-PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.kb_pad_port_l=11
+PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.kb_pad_port_r=4
+PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.kb_pad_port_l=4
 
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \
