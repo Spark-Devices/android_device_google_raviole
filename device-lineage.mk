919897b80cce3f3b432b4dd37765b26d3495d4e9
Matt Filetto <matt.filetto@gmail.com>
2022-04-17 04:07:34 -0700

    Set camera extension package to Google package
    Change-Id: I519ebf8a1e2a9d06561471206c91c01b352ebd47
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: Ic65fc5165d72dfd982af4ac174083b121ff1296d

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,6 +1,11 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/raviole/overlay-lineage
 
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
