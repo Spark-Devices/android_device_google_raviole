47f937bf33180f2f777c0b595e2f1c7187f42deb
Matt Filetto <matt.filetto@gmail.com>
2022-04-17 03:51:01 -0700

    raviole: StatiXOS -> LineageOS
    Change-Id: I15355708b3458848a6e4543b0234d7a25da8cc1e

--- lineage_oriole.mk
+++ lineage_oriole.mk
@@ -2,16 +2,15 @@
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common StatiX stuff.
-$(call inherit-product, vendor/statix/config/common.mk)
-$(call inherit-product, vendor/statix/config/gsm.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_oriole_64.mk)
-$(call inherit-product, device/google/raviole/device-statix.mk)
+$(call inherit-product, device/google/raviole/device-lineage.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := statix_oriole
+PRODUCT_NAME := lineage_oriole
 PRODUCT_MODEL := Pixel 6
 PRODUCT_BRAND := google
 
